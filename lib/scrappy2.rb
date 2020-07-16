fc99f61b15c419836f22f74f79232ba21cb7077f
fauvel <gorm.t@orange.fr>
2020-07-16 23:09:14 +0200

    désespoir

--- lib/scrappy2.rb
+++ lib/scrappy2.rb
@@ -1,19 +1,24 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
+   
 
-def creppy
+def creppy 
     page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
-
 
 	crypto_name = page.xpath('//td[@class="cmc-table__cell cmc-table__cell--sortable cmc-table__cell--left cmc-table__cell--sort-by__symbol"]')
 	crypto_price = page.xpath('//td[@class="cmc-table__cell cmc-table__cell--sortable cmc-table__cell--right cmc-table__cell--sort-by__price"]')
 
-    crypto_array = []
+	crypto_array = []
+	
+	cryp = Hash.new
 
-    for i in 0..(crypto_name.length - 1)
-        crypto_array << {crypto_name[i].text => crypto_price[i].text}
-    end
-    return crypto_array
+	for i in 0..(crypto_name.length - 1)
+		crypto_array << {crypto_name[i].text => crypto_price[i].text}
+	end
+	return crypto_array
 end
-print creppy
+puts creppy
+
+
+    
